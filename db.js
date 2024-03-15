77907735c8f605b6324047223294642d8f554155
Abhishek Desale <abhishekdesale5@gmail.com>
2024-03-15 23:37:13 +0530

    initialcommit

--- db.js
+++ db.js
@@ -1,17 +1,15 @@
 const mongoose = require ('mongoose');
 const mongoURL = 'mongodb+srv://abhishekdesale5:Abhishek@cluster0.80qsv.mongodb.net'
 
-mongoose.connect('mongodb+srv://abhishekdesale5:Abhishek@cluster0.80qsv.mongodb.net')
+mongoose.connect('mongodb+srv://abhishekdesale5:Abhishek@cluster0.80qsv.mongodb.net/FoodDelivery')
 .then(()=> console.log('Mongodb is connected'))
 .catch((e)=> console.log(e))
 
 //Mongose maintain default connection object to represent mongodb connection
 const db = mongoose.connection;
 // connection establishedment using event listner
-db.on( 'connected',async ()=>{
+db.on( 'connected', ()=>{
     console.log("Mongo is connected");
-    const admin = db.db.admin();
-    console.log(await admin.listDatabases());
 });
 db.on('error',(err)=>{
     console.log("Mongodb connection error", err);
